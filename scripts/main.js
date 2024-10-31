a8e227ae67a34216ceae1f7a8236c379872c2bb6
fod1x <106670659+DmitriyKolt@users.noreply.github.com>
2024-10-31 16:18:21 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,26 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    effect: 'fade',
+const swiperTop = new Swiper('.top__swiper', {
+  // Optional parameters
+  effect: 'fade',
 
-    autoplay: {
-        delay: 4500,
-        disableOnInteraction: false,
-    },
-  
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+
+const swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
